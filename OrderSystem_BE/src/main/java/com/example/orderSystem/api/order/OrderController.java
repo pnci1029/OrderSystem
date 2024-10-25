4606422c76933019f5f77724fbca3ddc9f91ca22
chhong <pnci1029@gmail.com>
2024-10-25 16:12:48 +0900

    [feature] add websocket api

--- OrderSystem_BE/src/main/java/com/example/orderSystem/api/order/OrderController.java
+++ OrderSystem_BE/src/main/java/com/example/orderSystem/api/order/OrderController.java
@@ -2,6 +2,7 @@ package com.example.orderSystem.api.order;
 
 import com.example.orderSystem.api.order.dto.OrderCreateRequestDto;
 import com.example.orderSystem.util.Response;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,14 +12,14 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/order")
-    public Response<?> createOrder(@RequestBody OrderCreateRequestDto dto) {
+    public Response<?> createOrder(@RequestBody OrderCreateRequestDto dto, HttpServletRequest request) {
         orderService.createOrder(dto);
         return Response.ok();
     }
 
     @GetMapping("/order")
     public Response<?> getOrders() {
-        ;
         return Response.ok(orderService.getOrders());
     }
+
 }
--- OrderSystem_BE/src/main/java/com/example/orderSystem/api/order/OrderService.java
+++ OrderSystem_BE/src/main/java/com/example/orderSystem/api/order/OrderService.java
@@ -3,20 +3,28 @@ package com.example.orderSystem.api.order;
 import com.example.orderSystem.api.order.dto.OrderCreateRequestDto;
 import com.example.orderSystem.domain.order.Order;
 import com.example.orderSystem.domain.order.OrderRepository;
+import com.example.orderSystem.util.OrderWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
 
 import java.util.List;
 
 @Service @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final OrderWebSocketHandler orderWebSocketHandler;
 
     public void createOrder(OrderCreateRequestDto dto) {
         orderRepository.save(dto);
+
+        List<Order> result = orderRepository.findAll();
+        orderWebSocketHandler.broadcastOrder(result);
     }
 
     public List<Order> getOrders() {
         return orderRepository.findAll();
     }
+
+
 }
