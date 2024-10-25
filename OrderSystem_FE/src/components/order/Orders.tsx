910eeb1dd1560ee6b4aa19bd72f1d3cd5cc9ad2d
chhong <pnci1029@gmail.com>
2024-10-25 18:30:52 +0900

    [feature] fix be code & add websocket connection in fe

--- OrderSystem_FE/src/components/order/Orders.tsx
+++ OrderSystem_FE/src/components/order/Orders.tsx
@@ -1,13 +1,15 @@
 import {useEffect, useState} from "react";
+import {Order} from "../../types/order";
+import style from "../../style/order/order.module.scss"
 
 export function Orders() {
-    const [orders, setOrders] = useState([]);
+    const [orders, setOrders] = useState<Order[]>([]);
 
     useEffect(() => {
         const socket = new WebSocket('ws://localhost:8080/ws/orders');
 
         socket.addEventListener('open',(event) =>{
-            socket.send('hellowwwww')
+            socket.send('websocket is Open')
         })
 
         socket.addEventListener('message',(event) =>{
@@ -15,16 +17,34 @@ export function Orders() {
         })
 
         socket.onmessage = (event) => {
-            const newOrder = event.data;
-            console.log(newOrder)
-            // setOrders((prevOrders) => [...prevOrders, newOrder]);
+            const newOrders: Order[] = JSON.parse(event.data);
+            setOrders((newOrders));
         };
 
         return () => socket.close();
     }, []);
+    console.log(orders)
+    console.log(orders[0])
     return(
         <>
-            하이
+            <div className={style.orderTitleWrapper}>
+                <span className={style.orderTitle}>
+                    실시간 주문 현황
+                </span>
+            </div>
+
+            <div className={style.ordersWrapper}>
+                {orders.map((data, index) =>(
+                    <div className={style.orderItems} key={index}>
+                        <p className={style.text}>
+                            주문 메뉴 이름 : {data.name}
+                        </p>
+                        <p className={style.text}>
+                            주문 수량 : {data.quantity}
+                        </p>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
--- OrderSystem_FE/src/types/order.ts
+++ OrderSystem_FE/src/types/order.ts
@@ -2,3 +2,9 @@ export interface OrderCreateRequestDto{
     name: string;
     quantity: number;
 }
+
+export interface Order{
+    id: number;
+    name: string;
+    quantity: number;
+}
