0889ca3b33093d06beadf6c2a8622bab9aab553c
chhong <pnci1029@gmail.com>
2024-10-26 15:18:32 +0900

    [feature] add status in UI

--- OrderSystem_FE/src/types/order.ts
+++ OrderSystem_FE/src/types/order.ts
@@ -6,5 +6,12 @@ export interface OrderCreateRequestDto{
 export interface Order{
     id: number;
     name: string;
+    status: Status;
     quantity: number;
+}
+
+export enum Status{
+    RECEIVED="접수됨",
+    PROCESSING="처리 중",
+    FINISHED="처리 완료"
 }
