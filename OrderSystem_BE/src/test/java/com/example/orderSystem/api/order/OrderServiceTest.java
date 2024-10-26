9adc9ec0447c9662fbe31510fce584a6d29001a3
chhong <pnci1029@gmail.com>
2024-10-26 16:03:59 +0900

    [feature] add service test about update

--- OrderSystem_BE/src/test/java/com/example/orderSystem/api/order/OrderServiceTest.java
+++ OrderSystem_BE/src/test/java/com/example/orderSystem/api/order/OrderServiceTest.java
@@ -2,12 +2,17 @@ package com.example.orderSystem.api.order;
 
 import com.example.orderSystem.api.order.dto.OrderCreateRequestDto;
 import com.example.orderSystem.api.order.dto.OrderResponseDto;
+import com.example.orderSystem.domain.order.Order;
 import com.example.orderSystem.domain.order.OrderRepository;
+import com.example.orderSystem.domain.order.Status;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,8 +47,39 @@ class OrderServiceTest {
         OrderResponseDto response = orderService.createOrder(request);
 
         // then
+        // 응답받은 데이터와 요청받은 데이터가 일치하는지 확인
         assertThat(response)
                 .extracting("name","quantity")
                 .contains(name, quantity);
+    }
+
+    @DisplayName("주문 ID를 받고, 다음 주문 상태로 업데이트 한다.")
+    @Test
+    void updateOrderServiceTest() {
+        // given
+        String name = "탕수육";
+        int quantity = 2;
+        OrderCreateRequestDto request = OrderCreateRequestDto
+                .builder()
+                .name(name)
+                .quantity(quantity)
+                .build();
+
+        OrderResponseDto createdOrder = orderService.createOrder(request);
+        Order order = orderService.getOrders().stream().findFirst().orElseThrow();
+
+        // when
+        OrderResponseDto firstUpdatedOrder = orderService.updateOrder(order.getId());
+        OrderResponseDto secondUpdatedOrder = orderService.updateOrder(order.getId());
+
+        // then
+        // 상태가 초기 상태와 다른지 확인
+        assertThat(createdOrder.getStatus()).isNotEqualTo(firstUpdatedOrder.getStatus());
+
+        // 상태가 RECEIVED에서 PROCESSING으로 변경되었는지 확인
+        assertThat(firstUpdatedOrder.getStatus()).isEqualTo(Status.PROCESSING);
+
+        // 상태가 PROCESSING에서 FINISHED로 변경되었는지 확인
+        assertThat(secondUpdatedOrder.getStatus()).isEqualTo(Status.FINISHED);
     }
 }
