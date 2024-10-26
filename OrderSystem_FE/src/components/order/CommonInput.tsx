a280e7d6d2da4427a7ccf7f08ee5eed4ebf29a3e
chhong <pnci1029@gmail.com>
2024-10-26 14:03:30 +0900

    [feature] add new status for errorMessage in order inputs

--- OrderSystem_FE/src/components/order/CommonInput.tsx
+++ OrderSystem_FE/src/components/order/CommonInput.tsx
@@ -5,14 +5,15 @@ interface Args{
     type: OrderTypes;
     value: any;
     setValue: (_: any) => void
+    errorMessage: string;
 }
-export function CommonInput({type, value, setValue}:Args) {
+export function CommonInput({type, value, setValue, errorMessage}:Args) {
 
     // 라벨명, 인풋 타입 정의
     const renderInputField = () => {
         const inputProps = {
-            MENU: {label: '메뉴 이름', type: 'text'},
-            QUANTITY: {label: '수량', type: 'number'},
+            MENU: {label: '메뉴 이름', type: 'text', errorMessage:errorMessage},
+            QUANTITY: {label: '수량', type: 'number',errorMessage:errorMessage},
         };
 
         const {label, type: inputType} = inputProps[type] || {};
@@ -23,8 +24,8 @@ export function CommonInput({type, value, setValue}:Args) {
                 <input className={style.inputStyle} type={inputType}
                        value={value}
                        onChange={(e) => setValue(inputType === 'number' ? Number(e.target.value) : e.target.value)}
-
                 />
+                <p className={style.errorMessage}>{errorMessage}</p>
             </div>
         ) : null;
     };
--- OrderSystem_FE/src/components/order/CreateOrder.tsx
+++ OrderSystem_FE/src/components/order/CreateOrder.tsx
@@ -3,28 +3,46 @@ import {CommonInput} from "./CommonInput";
 import {OrderTypes} from "../../types/common";
 import {useCallback, useState} from "react";
 import {useDispatch} from "react-redux";
-import {createOrder, getMenus} from "../../slice/orderSlice";
+import {createOrder} from "../../slice/orderSlice";
+import {useOrderValidator} from "./hooks/useOrderValidator";
+import {useNavigate} from "react-router-dom";
 
 
 export function CreateOrder() {
+    const dispatch = useDispatch<any>();
+    const navigate = useNavigate()
     const [menu, setMenu] = useState('');
     const [quantity, setQuantity] = useState(0);
-    const dispatch = useDispatch<any>();
 
+    const [menuErrorMessage, setMenuErrorMessage] = useState('');
+    const [quantityErrorMessage, setQuantityErrorMessage] = useState('');
+
+    const {validate} = useOrderValidator({menu, quantity, setMenuErrorMessage, setQuantityErrorMessage});
 
     // 주문 생성 API 요청
     const handleCreateOrder = useCallback(async () => {
 
+        const result = validate();
+        console.log(result)
+
+        if (!result) return false;
+
         try {
-            const result: any = await dispatch(createOrder({name:menu, quantity:quantity})).unwrap();
-            console.log(result)
+            const result: any = await dispatch(createOrder({name: menu, quantity: quantity}))
+                .unwrap()
+                .then(() => {
+                    alert('주문이 접수되었습니다.');
+                    navigate('/order')
+                });
 
             return result;
         } catch (e) {
             console.log('error message : ', e)
+            alert('일시적인 문제가 발생했습니다. 다시 시도해주세요.')
         }
         // eslint-disable-next-line
     }, [menu, quantity])
+
     return (
         <>
             {/* 주문 헤더 영역 */}
@@ -36,13 +54,15 @@ export function CreateOrder() {
 
             {/* 주문 폼 영역 */}
             <div className={style.orderBodyWrapper}>
-                <CommonInput type={OrderTypes.MENU} value={menu} setValue={setMenu}/>
-                <CommonInput type={OrderTypes.QUANTITY} value={quantity} setValue={setQuantity}/>
+                <CommonInput type={OrderTypes.MENU} value={menu} setValue={setMenu}
+                             errorMessage={menuErrorMessage}/>
+                <CommonInput type={OrderTypes.QUANTITY} value={quantity} setValue={setQuantity}
+                             errorMessage={quantityErrorMessage}/>
             </div>
 
             {/* 주문 버튼 영역 */}
             <div className={style.buttonsWrapper}>
-                <button className={style.createButton} onClick={handleCreateOrder}>등록하기</button>
+                <button className={style.createButton} onClick={handleCreateOrder}>주문</button>
             </div>
         </>
     )
